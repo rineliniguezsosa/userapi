863adde2fd6b4d2882d9d675502eef3ea1e13acf
Rinel <rineliniguezsosa@gmail.com>
2025-03-14 23:46:12 -0600

    [FEAT] Multiples cambios en el archivo usuario.model.ts
    - importamos la interface Usuario
    - creamos el schema user para modelar nuestro documento
    - exportamos el usuario el cual transformamos el schema user a un modelo

--- models/usuario.model.ts
+++ models/usuario.model.ts
@@ -1,1 +1,42 @@
+import { Usuario } from './../types/interfaces';
 import { model,Schema } from 'mongoose';
+
+const user :Schema<Usuario> = new Schema({
+    gender:{
+        type:String
+    },
+    name:{
+        type:String
+    },
+    location:{
+        street:{
+            number:{
+                type:Number
+            },
+            name:{
+                type:String
+            }
+        },
+        city:{
+            type:String
+        },
+        state:{
+            type:String
+        },
+        country:{
+            type:String
+        }, 
+        postcode:{
+            type:String || Number
+        }
+
+    },
+    email:{
+        type:String
+    },
+    nat:{
+        type:String
+    }
+})
+
+export const usuario = model<Usuario>('usuario',user);
