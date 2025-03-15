4dee58edbf2d61d213c48e08803ca21849e30ea3
Rinel <rineliniguezsosa@gmail.com>
2025-03-15 01:31:59 -0600

    [FEAT] Creé la ruta "/get/all" para obtener todo los registros y además estableci el método getUsers como middleware

--- routes/usuario.router.ts
+++ routes/usuario.router.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
-import { getListUsers } from '../controllers';
+import { getListUsers,getUsers } from '../controllers';
 
 export const usuarioRouter = Router();
 
 usuarioRouter.get('/get/userlist',getListUsers)
+usuarioRouter.get('/get/all',getUsers)
