647c8d6a7c26a0dffe9c4893260a0165e288d4f3
Rinel <rineliniguezsosa@gmail.com>
2025-03-15 02:07:57 -0600

    [FEAT} Creando la ruta "/delete" para eliminar los  registros de los usuarios
    - en la ruta delete se ejecutara el middleware deleteuser

--- routes/usuario.router.ts
+++ routes/usuario.router.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { getListUsers,getUsers } from '../controllers';
+import { getListUsers,getUsers,deleteUser } from '../controllers';
 
 export const usuarioRouter = Router();
 
 usuarioRouter.get('/get/userlist',getListUsers)
 usuarioRouter.get('/get/all',getUsers)
+usuarioRouter.delete('/delete',deleteUser)
