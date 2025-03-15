19af7912bc3c31ea68352361be77c9861b4393f4
Rinel <rineliniguezsosa@gmail.com>
2025-03-15 00:54:40 -0600

    [FEAT] Multiples cambios en el método getListUsers
    - importando el modelo usuario
    - importando la interface  UsuarioRequest para tipar la solicitud http
    - cambiando la variable usuarios por req y tipando la solicitud http get
    - cambiando la variable resp por res y cambiando la variable usuario por req
    - creé la variable data en donde mapeé los results ya que ahi se contenida mi data, me creé un objeto para almacenar toda información por guardar del modelo de nuestro usuario
    - creé una variable user en donde realice una inserción de muchos registros gracias al insertMany en donde estableci la opción ordered false para que no inserte registros duplicados
    - devolvi las respectivas respuestas para los casos de exitos y de error
    - estableci un log para observar los errores

--- controllers/usuario.controller.ts
+++ controllers/usuario.controller.ts
@@ -1,13 +1,43 @@
 import {Request,Response} from 'express'
 import axios from 'axios';
+import { usuario } from '../models';
+import { UsuarioRequest } from '../types/interfaces';
 
-export const getListUsers = async(req:Request,resp:Response) =>{
+export const getListUsers = async(requ:Request,resp:Response) =>{
     try {
-        const usuarios = await axios.get(`${process.env.RANDOMUSER}`);
-        console.log("usuarios: ",usuarios);
-        const resp = await usuarios.data;
+        const req = await axios.get<UsuarioRequest>(`${process.env.RANDOMUSER}`);
+        console.log("usuarios: ",req);
+        const res = await req.data;
+
+        const data = res.results.map((user)=> ({
+            gender: user.gender,
+            name: user.name,
+            location:{
+                street:{
+                    number:user.location.street.number,
+                    name:user.location.street.name
+                },
+                city: user.location.city,
+                state: user.location.state,
+                country: user.location.country,
+                postcode: user.location.postcode
+            }
+        }))
+        console.log("respuesta",resp);
+        const user = await usuario.insertMany(data,{ ordered: false })
+
+        resp.json({
+            message:'Datos insertados',
+            status:true,
+            data: user
+        })
+
         
     } catch (error) {
-        
+        console.log('error al insertar: ',error);
+        resp.json({
+            message:'Datos no ingresados',
+            status:false
+        })
     }
 }
