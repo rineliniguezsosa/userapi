a66be2bc0e73babe9e2d30717de15e08930ff9b2
Rinel <rineliniguezsosa@gmail.com>
2025-03-14 23:11:11 -0600

    [FEAT] Cambios en el método mongoconnection
    - estableci un try catch para verificar la conexión de mongo
    - use el wait para esperar la respuesta de la conexión de mongo
    - estableci logs para ver errores y la conexión exitosa de mongo

--- config/connectiondb/mongoconnection.ts
+++ config/connectiondb/mongoconnection.ts
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
 
 export const mongoconnection = async() =>{
-    
+    try {
+        await mongoose.connect(`${process.env.MONGOURL}`);
+        console.log("Conexión exitosa a la base de datos.")
+    } catch (error) {
+        console.error('Error de conexión a la base de datos:',error);
+    }
 }
