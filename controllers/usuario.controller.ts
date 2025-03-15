24d4ed319d842bf5d76c0e9e832034264d12847a
Rinel <rineliniguezsosa@gmail.com>
2025-03-15 00:11:56 -0600

    [FEAT] Cambios en el método getListUsers
    - importando axios
    - creé la variable usuarios en donde esperé la respuesta de obtener una lista de usuarios de una api
    - establezco un log para observar el valor de usuarios

--- controllers/usuario.controller.ts
+++ controllers/usuario.controller.ts
@@ -1,7 +1,10 @@
 import {Request,Response} from 'express'
+import axios from 'axios';
 
 export const getListUsers = async(req:Request,resp:Response) =>{
     try {
+        const usuarios = await axios.get(`${process.env.RANDOMUSER}`);
+        console.log("usuarios: ",usuarios);
         
     } catch (error) {
         
