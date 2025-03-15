0f43d3157ac28a6ea2e27b526e5224ab0cb5c0e7
Rinel <rineliniguezsosa@gmail.com>
2025-03-14 23:12:54 -0600

    [FEAT] Cambios en el archivo server.ts
    - importe el método mongoconnection
    - llamé al método mongoconnection en el servidor para comprobar la conexión

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import 'dotenv/config';
+import { mongoconnection } from '../config';
 
 const app = express()
 
 app.use(express.json());
+
+mongoconnection();
 
 app.listen(process.env.PORT,()=>{
     console.log(`Servidor ejecutandose en http://localhost:${process.env.PORT}`);
