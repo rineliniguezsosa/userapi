4bd762e7d52c31b85af9adc607895813e9434cd1
Rinel <rineliniguezsosa@gmail.com>
2025-03-15 00:15:37 -0600

    [FEAT] Creé la variable resp en donde esperé la conversión de la información de la api en formato json

--- controllers/usuario.controller.ts
+++ controllers/usuario.controller.ts
@@ -5,6 +5,7 @@ export const getListUsers = async(req:Request,resp:Response) =>{
     try {
         const usuarios = await axios.get(`${process.env.RANDOMUSER}`);
         console.log("usuarios: ",usuarios);
+        const resp = await usuarios.data;
         
     } catch (error) {
         
