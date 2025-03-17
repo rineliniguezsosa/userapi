1bcb6202a64ec0a7e5290940800bb8493d56bc75
Rinel <rineliniguezsosa@gmail.com>
2025-03-17 17:26:11 -0600

    [FEAT] Creando la ruta "/save" de tipo post para la creación de nuevos usuarios , importe el método saveUser en donde en la ruta "save" se ejecutará este controlador

--- routes/usuario.router.ts
+++ routes/usuario.router.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { getListUsers,getUsers,deleteUser } from '../controllers';
+import { getListUsers,getUsers,deleteUser,saveUser } from '../controllers';
 
 export const usuarioRouter = Router();
 
 usuarioRouter.get('/get/userlist',getListUsers)
 usuarioRouter.get('/get/all',getUsers)
 usuarioRouter.delete('/delete/:_id',deleteUser)
+usuarioRouter.post('/save',saveUser)
